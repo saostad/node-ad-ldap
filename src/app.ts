36b8664bb5fa9b77c4b0475f905752043ed02f9e
Saeid Ostad <SOstad@kajimausa.com>
2019-11-08 13:18:53 -0500

    in middle

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import dotenv from "dotenv";
 import { IClientConfig, AdClient } from "./client";
+import { Group } from "./group";
 dotenv.config();
 
 const bindDN = process.env.AD_USER;
@@ -8,15 +9,18 @@ const secret = process.env.AD_PASS;
 export const baseDN = "dc=KI,dc=Local";
 
 async function main() {
-  const config: IClientConfig = {
-    url: "ldap://ki.local",
-    bindDN,
-    secret,
-    baseDN,
-  };
-  const adClient = new AdClient(config);
-  const groups = await adClient.getGroupMembershipForUser("sostad");
-  groups.map(el => console.log(`File: app.ts,`, `Line: 14 => `, el));
+  //   const config: IClientConfig = {
+  //     url: "ldap://ki.local",
+  //     bindDN,
+  //     secret,
+  //     baseDN,
+  //   };
+  //   const adClient = new AdClient(config);
+  //   const groups = await adClient.getGroupMembershipForUser("sostad");
+  //   groups.map(el => console.log(`File: app.ts,`, `Line: 14 => `, el));
+
+  const g1 = new Group({ Parent: "Ali" });
+  console.log(`File: app.ts,`, `Line: 23 => `, g1);
 }
 
 main();
--- src/group.ts
+++ src/group.ts
@@ -64,17 +64,75 @@ type GroupFieldsName = {
 };
 
 export interface GroupFields extends GroupFieldsName {
-  telephoneNumber?: number;
+  telephoneNumber?: string;
 }
 
 export class Group implements GroupFields {
+  adminDescription?: string;
+  adminDisplayName?: string;
+  ADsPath?: string;
+  authOrig?: string;
+  authOrigBL?: string;
+  cn?: string;
+  createTimeStamp?: string;
+  delivContLength?: string;
+  descritpion?: string;
+  displayName?: string;
+  displayNamePrintable?: string;
+  distinguedName?: string;
+  dLMemRejectPerms?: string;
+  dLMemRejectPermsBL?: string;
+  dLMemSubmitPerms?: string;
+  dLMemSubmitPermsBL?: string;
+  extensionAttribute?: string;
+  groupType?: string;
+  homeMTA?: string;
+  info?: string;
+  isDeleted?: string;
+  legacyExchangeDN?: string;
+  mail?: string;
+  mailNickName?: string;
+  managedBy?: string;
+  member?: string;
+  memberOf?: string;
+  modifyTimeStamp?: string;
+  msExchExpansionServerName?: string;
+  msExchHideFromAddressLists?: string;
+  msExchHomeServerName?: string;
+  msExchRequireAuthToSendTo?: string;
+  msSFU30GidNumber?: string;
+  msSFU30Name?: string;
+  msSFU30NisDomain?: string;
+  msSFU30PosixMember?: string;
+  name?: string;
+  Name?: string;
+  nTSecurityDescriptor?: string;
+  objectCategory?: string;
+  objectClass?: string;
+  objectGUID?: string;
+  objectSid?: string;
+  oOFReplyToOriginator?: string;
+  Parent?: string;
+  primaryGroupToken?: string;
+  proxyAddresses?: string;
+  reportToOriginator?: string;
+  reportToOwner?: string;
+  sAMAccountName?: string;
+  telephoneNumber?: string;
+  textEncodedORAddress?: string;
+  unauthOrig?: string;
+  unauthOrigBL?: string;
+  uSNChanged?: string;
+  uSNCreated?: string;
+  whenChanged?: string;
+  whenCreated?: string;
+
   constructor(fields?: GroupFields) {
     if (fields) {
-      for (const i of fields) {
-        // this.[name] = fields.name;
+      for (const [key, value] of Object.entries(fields)) {
+        this.[key] = value;
       }
     }
   }
 }
 
-const g1 = new Group();
