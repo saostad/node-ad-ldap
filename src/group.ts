ee89148cdbda87c99ac70d87650e816856ce1a6a
Saeid Ostad <SOstad@kajimausa.com>
2019-11-20 14:23:35 -0500

    - v 1.0.0 - packages updated - Breaking: for attribute values with 1 value return that value in string format and other values in array format

--- src/group.ts
+++ src/group.ts
@@ -67,72 +67,76 @@ type GroupFieldsName = {
 };
 
 export interface GroupFields extends GroupFieldsName {
-  telephoneNumber?: string[];
+  telephoneNumber?: string[] | string;
 }
 
 export class Group implements GroupFields {
-  readonly adminDescription?: string[];
-  readonly adminDisplayName?: string[];
-  readonly ADsPath?: string[];
-  readonly authOrig?: string[];
-  readonly authOrigBL?: string[];
-  readonly cn?: string[];
-  readonly createTimeStamp?: string[];
-  readonly delivContLength?: string[];
-  readonly descritpion?: string[];
-  readonly displayName?: string[];
-  readonly displayNamePrintable?: string[];
-  readonly distinguedName?: string[];
-  readonly dLMemRejectPerms?: string[];
-  readonly dLMemRejectPermsBL?: string[];
-  readonly dLMemSubmitPerms?: string[];
-  readonly dLMemSubmitPermsBL?: string[];
-  readonly extensionAttribute?: string[];
-  readonly groupType?: string[];
-  readonly homeMTA?: string[];
-  readonly info?: string[];
-  readonly isDeleted?: string[];
-  readonly legacyExchangeDN?: string[];
-  readonly mail?: string[];
-  readonly mailNickName?: string[];
-  readonly managedBy?: string[];
-  readonly member?: string[];
-  readonly memberOf?: string[];
-  readonly modifyTimeStamp?: string[];
-  readonly msExchExpansionServerName?: string[];
-  readonly msExchHideFromAddressLists?: string[];
-  readonly msExchHomeServerName?: string[];
-  readonly msExchRequireAuthToSendTo?: string[];
-  readonly msSFU30GidNumber?: string[];
-  readonly msSFU30Name?: string[];
-  readonly msSFU30NisDomain?: string[];
-  readonly msSFU30PosixMember?: string[];
-  readonly name?: string[];
-  readonly Name?: string[];
-  readonly nTSecurityDescriptor?: string[];
-  readonly objectCategory?: string[];
-  readonly objectClass?: string[];
-  readonly objectGUID?: string[];
-  readonly objectSid?: string[];
-  readonly oOFReplyToOriginator?: string[];
-  readonly Parent?: string[];
-  readonly primaryGroupToken?: string[];
-  readonly proxyAddresses?: string[];
-  readonly reportToOriginator?: string[];
-  readonly reportToOwner?: string[];
-  readonly sAMAccountName?: string[];
-  readonly telephoneNumber?: string[];
-  readonly textEncodedORAddress?: string[];
-  readonly unauthOrig?: string[];
-  readonly unauthOrigBL?: string[];
-  readonly uSNChanged?: string[];
-  readonly uSNCreated?: string[];
-  readonly whenChanged?: string[];
-  readonly whenCreated?: string[];
+  readonly adminDescription?: string[] | string;
+  readonly adminDisplayName?: string[] | string;
+  readonly ADsPath?: string[] | string;
+  readonly authOrig?: string[] | string;
+  readonly authOrigBL?: string[] | string;
+  readonly cn?: string[] | string;
+  readonly createTimeStamp?: string[] | string;
+  readonly delivContLength?: string[] | string;
+  readonly descritpion?: string[] | string;
+  readonly displayName?: string[] | string;
+  readonly displayNamePrintable?: string[] | string;
+  readonly distinguedName?: string[] | string;
+  readonly dLMemRejectPerms?: string[] | string;
+  readonly dLMemRejectPermsBL?: string[] | string;
+  readonly dLMemSubmitPerms?: string[] | string;
+  readonly dLMemSubmitPermsBL?: string[] | string;
+  readonly extensionAttribute?: string[] | string;
+  readonly groupType?: string[] | string;
+  readonly homeMTA?: string[] | string;
+  readonly info?: string[] | string;
+  readonly isDeleted?: string[] | string;
+  readonly legacyExchangeDN?: string[] | string;
+  readonly mail?: string[] | string;
+  readonly mailNickName?: string[] | string;
+  readonly managedBy?: string[] | string;
+  readonly member?: string[] | string;
+  readonly memberOf?: string[] | string;
+  readonly modifyTimeStamp?: string[] | string;
+  readonly msExchExpansionServerName?: string[] | string;
+  readonly msExchHideFromAddressLists?: string[] | string;
+  readonly msExchHomeServerName?: string[] | string;
+  readonly msExchRequireAuthToSendTo?: string[] | string;
+  readonly msSFU30GidNumber?: string[] | string;
+  readonly msSFU30Name?: string[] | string;
+  readonly msSFU30NisDomain?: string[] | string;
+  readonly msSFU30PosixMember?: string[] | string;
+  readonly name?: string[] | string;
+  readonly Name?: string[] | string;
+  readonly nTSecurityDescriptor?: string[] | string;
+  readonly objectCategory?: string[] | string;
+  readonly objectClass?: string[] | string;
+  readonly objectGUID?: string[] | string;
+  readonly objectSid?: string[] | string;
+  readonly oOFReplyToOriginator?: string[] | string;
+  readonly Parent?: string[] | string;
+  readonly primaryGroupToken?: string[] | string;
+  readonly proxyAddresses?: string[] | string;
+  readonly reportToOriginator?: string[] | string;
+  readonly reportToOwner?: string[] | string;
+  readonly sAMAccountName?: string[] | string;
+  readonly telephoneNumber?: string[] | string;
+  readonly textEncodedORAddress?: string[] | string;
+  readonly unauthOrig?: string[] | string;
+  readonly unauthOrigBL?: string[] | string;
+  readonly uSNChanged?: string[] | string;
+  readonly uSNCreated?: string[] | string;
+  readonly whenChanged?: string[] | string;
+  readonly whenCreated?: string[] | string;
 
   setFields(fields: GroupFields) {
     for (const [key, value] of Object.entries(fields)) {
-      this[key] = value;
+      if (Array.isArray(value) && value.length > 1) {
+        this[key] = value as string[];
+      } else {
+        this[key] = value[0] as string;
+      }
     }
     return this;
   }
