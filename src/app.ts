5293ea31de287edd4782d284aab0c22aa92240d6
Saeid Ostad <SOstad@kajimausa.com>
2020-01-24 12:50:57 -0500

    removed extra code.

--- src/app.ts
+++ src/app.ts
@@ -1,30 +1,30 @@
 export * from "./ad-client";
 
 /** HOW TO USE */
-import { AdClient } from "./ad-client";
-import { config } from "dotenv";
-import { createLogger, writeLog } from "fast-node-logger";
-config();
+// import { AdClient } from "./ad-client";
+// import { config } from "dotenv";
+// import { createLogger, writeLog } from "fast-node-logger";
+// config();
 
-let adClient: AdClient;
+// let adClient: AdClient;
 
-async function main() {
-  const logger = await createLogger();
+// async function main() {
+//   const logger = await createLogger();
 
-  adClient = new AdClient({
-    bindDN: process.env.AD_USER,
-    secret: process.env.AD_Pass,
-    url: process.env.AD_URI,
-    baseDN: "dc=KI,dc=Local",
-    logger,
-  });
+//   adClient = new AdClient({
+//     bindDN: process.env.AD_USER,
+//     secret: process.env.AD_Pass,
+//     url: process.env.AD_URI,
+//     baseDN: "dc=KI,dc=Local",
+//     logger,
+//   });
 
-  const data = await adClient
-    .findGroup("Group_7753ae4a-1a6e-4294-a5f8-eca521679dd6")
-    .catch(err => writeLog(err, { stdout: true }));
+//   const data = await adClient
+//     .findGroup("Group_7753ae4a-1a6e-4294-a5f8-eca521679dd6")
+//     .catch(err => writeLog(err, { stdout: true }));
 
-  console.log(`File: app.ts,`, `Line: 9 => `, data);
-}
-main().finally(() => {
-  adClient.unbind();
-});
+//   console.log(`File: app.ts,`, `Line: 9 => `, data);
+// }
+// main().finally(() => {
+//   adClient.unbind();
+// });
